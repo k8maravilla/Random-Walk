21f006379ed15ee8df213a2df0fc31f0c2adebe7
Kaleb Maravilla <=>
2023-04-16 21:51:03 -0600

    first commit

---
+++ randum.py
@@ -0,0 +1,14 @@
+import turtle
+import statistics
+
+t = turtle.Turtle()
+
+def simulate(walk_lengths, trials, walkers):
+    """This function should simuate parameters and print a summary"""
+    
+    
+def plot():
+    """this function creates a visual of the simulate info"""
+    pass
+
+
