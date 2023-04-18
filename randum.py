b850840ae876e603695dbc34694d94a20aba55ff
Kaleb Maravilla <=>
2023-04-18 11:09:25 -0600

    did the lab challenge

--- randum.py
+++ randum.py
@@ -1,11 +1,12 @@
 import turtle
 import statistics
+import random
 
 t = turtle.Turtle()
 
 def simulate(walk_lengths, trials, walkers):
     """This function should simuate parameters and print a summary"""
-    
+    pass
     
 def plot():
     """this function creates a visual of the simulate info"""
---
+++ recursive.py
@@ -0,0 +1,10 @@
+
+#lab challenge -----------------------------------
+#def recursive_power(base, exp):
+#    if exp == 0:
+#        return 1
+#    else:
+#        return base * recursive_power(base, exp -1)
+    
+#print(recursive_power(5, 3))
+        
