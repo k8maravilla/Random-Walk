90c4089283d41ff2ea8c188616af5ad7b5659dda
Kaleb Maravilla <=>
2023-04-20 08:41:15 -0600

    updates to my simulate function

--- randum.py
+++ randum.py
@@ -4,24 +4,24 @@ import random
 
 t = turtle.Turtle()
 
-def Pa():
+def pa():
     '''randomly chooses which direction pa goes'''
     return random.choice([(0,1), (1,0), (0, -1), (-1, 0)])
 
-def Mi_Ma():
+def mi_ma():
     '''randomly chooses which direction Mi-Ma goes'''
     return random.choice([(0,1), (1,0), (0, -1), (0, -1), (-1, 0)])
 
-def Reg():
+def reg():
     '''randomly chooses which direction reg goes'''
     return random.choice([(1,0), (-1, 0)])
 
 def my_trials(length, name):
     """gives endpoint for one trial"""
     my_dict = {
-        "Pa": Pa,
-        "Mi-Ma":Mi_Ma,
-        "Reg":Reg
+        "Pa": pa,
+        "Mi-Ma":mi_ma,
+        "Reg":reg
     }
     x = 0
     y = 0
@@ -40,25 +40,32 @@ def simulate(walk_lengths, trials, walkers):
     """This function should simuate parameters and print a summary"""
     all_walkers = []
     if walkers == "all":
-        all_walkers = ['Pa', 'Mi-Ma', 'reg']
+        all_walkers = ['Pa', 'Mi-Ma', 'Reg']
     else:
         all_walkers.append(walkers)
 
-    for walker in all_walkers:
-        for i in walk_lengths:
-            
+    answer_dict = {}
 
+    for walker in all_walkers:
+        answer_dict[walker] = {}
+        for i in walk_lengths:
+            my_walks = walks(i, trials, walker)
+            answer_dict[walker][i] = my_walks
+    print(answer_dict)
+    
     
 
         
 def plot():
     """this function creates a visual of the simulate info"""
     pass
+    #simulate()
 
 def main():
     #simulate()
-    print(walks(100, 50, "Pa"))
+    #print(walks(100, 50, "Pa"))
+    simulate([10, 15], 5, "all")
 
-main()
-#if __name__ == "__main__":
-    #main()
+
+if __name__ == "__main__":
+    main()
--- recursive.py
+++ recursive.py
@@ -7,4 +7,16 @@
 #        return base * recursive_power(base, exp -1)
     
 #print(recursive_power(5, 3))
-        
+
+#lab challenge 3 --------------------------------
+
+def bunny_ears(integer):
+    """counts number of bunny ears"""
+    double_integer = integer + integer
+    for i in range(double_integer):
+        if i == 0:
+            return 0
+        else:
+            i += 2 -1
+    return i
+print(bunny_ears(0))
